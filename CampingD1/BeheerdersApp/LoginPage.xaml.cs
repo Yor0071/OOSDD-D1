1f25d6b71d89f2ae4a7e4f752438f9a61f2a88cc
ssjuuuul <113347678+ssjuuuul@users.noreply.github.com>
2024-11-20 15:01:01 +0100

    loginscherm

---
+++ CampingD1/BeheerdersApp/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+namespace BeheerdersApp;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+        ShowPasswordCheckbox.CheckedChanged += (s, e) =>
+        {
+            PasswordEntry.IsPassword = !e.Value;
+        };
+    }
+
+    private async void OnLoginClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Succes","welkom", "ok");
+    }
+
+
+}
