8bddc8c7901c8d99a69fe1161d85ddb85fbe1590
Yor0071 <yorickboonstra@gmail.com>
2024-11-20 11:19:10 +0100

    Startup

---
+++ CampingD1/BeheerdersApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BeheerdersApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
