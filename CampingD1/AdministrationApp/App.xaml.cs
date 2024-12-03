27a3ec6f4af3cf44b169d3a7279c3fe81fd6f880
Pedropetet <s1199811@student.windesheim.nl>
2024-12-03 17:38:58 +0100

    Plekken overzicht beheerder afgerond
    kijk nog wel ff naar de App.xaml.cs, want daar is de MainPage aangepast

--- CampingD1/AdministrationApp/App.xaml.cs
+++ CampingD1/AdministrationApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace AdministrationApp
         {
             InitializeDatabase();
             InitializeComponent();
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new CampingSpotList());
         }
 
         private void InitializeDatabase()
