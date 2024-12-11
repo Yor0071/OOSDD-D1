c54eb74d84844cf40dba92ebfc03e1f5735987f7
Yor0071 <yorickboonstra@gmail.com>
2024-12-11 10:37:21 +0100

    fixed }

--- CampingD1/AdministrationApp/ReservationList.xaml.cs
+++ CampingD1/AdministrationApp/ReservationList.xaml.cs
@@ -103,6 +103,8 @@ public partial class ReservationList : ContentPage
                 }
             }
         }
+    }
+
     private async void OnSearchButtonPressed(object sender, EventArgs e)
     {
         SearchBar searchBar = (SearchBar)sender;
