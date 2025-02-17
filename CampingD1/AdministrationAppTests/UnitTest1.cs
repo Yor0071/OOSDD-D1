05593071af0ba9effc770213b2eb4ea75c01006d
Derrin Beekman <derrin@beekman.eu>
2025-02-17 13:16:24 +0100

    feat: add tests projects for both apps

---
+++ CampingD1/AdministrationAppTests/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace AdministrationAppTests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+    }
+}
---
+++ CampingD1/ReservationAppTests/UnitTest1.cs
@@ -0,0 +1,7 @@
+namespace ReservationAppTests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+    }
+}
